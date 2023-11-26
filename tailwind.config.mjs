58a90e1ea034560d6f80206e434f753b6352d529
felipe-lad <felipe.ladino@letsdoitnow.us>
2023-11-26 15:19:53 -0500

    feat(pages): add home view

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,21 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue-50": "#f1f8fd",
+        "custom-blue-100": "#dfeffa",
+        "custom-blue-200": "#c5e3f8",
+        "custom-blue-300": "#9ed1f2",
+        "custom-blue-400": "#70b7ea",
+        "custom-blue-500": "#4e9ae3",
+        "custom-blue-600": "#397fd7",
+        "custom-blue-700": "#306ac5",
+        "custom-blue-800": "#2d57a0",
+        "custom-blue-900": "#294a7f",
+        "custom-blue-950": "#1e3050",
+      },
+    },
     screens: {
       desktop: "1152px",
       ...defaultTheme.screens,
