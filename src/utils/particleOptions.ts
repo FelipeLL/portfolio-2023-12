06b1c6d1bdc5e57db2b9243dfe3dfc8a49a27c08
felipe-lad <felipe.ladino@letsdoitnow.us>
2023-11-25 21:58:14 -0500

    feat(layout): add particles

---
+++ src/utils/particleOptions.ts
@@ -0,0 +1,69 @@
+import type { ISourceOptions } from "tsparticles-engine";
+
+export const options: ISourceOptions = {
+  fps_limit: 120,
+  fullScreen: {
+    zIndex: -1,
+  },
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#fff",
+    },
+    links: {
+      color: "#fff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      random: true,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
